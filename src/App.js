b804536d33ebd0eba7d6f828675ba9ac743be82a
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-06-10 16:43:01 -0600

    Navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component.jsx';
+import Home from './routes/home/home.component.jsx';
+import SignIn from './routes/sign-in/sign-in.component.jsx';
+
+const Shop = () => {
+  return <h1> I am the shop page </h1>;
+};
 
 const App = () => {
-  const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "women",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "men",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
-
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
