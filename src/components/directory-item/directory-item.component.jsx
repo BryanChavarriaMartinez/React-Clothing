ee0d8ad6a5eb3ca0e4b89ed33f5f7e870ba590c9
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-06-27 12:57:56 -0600

    finishing styled components, last touches

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,17 @@
-import './directory-item.styles.scss'
+import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
     const { imageUrl, title } = category;
 
     return (
-        <div className="directory-item-container">
-            <div className="background-image" style={{
-                backgroundImage: `url(${imageUrl})`,
-            }} />
-            <div className="body">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-            </div>
-        </div>
-    )
+      <DirectoryItemContainer>
+        <BackgroundImage className="background-image" imageUrl={imageUrl} />
+        <Body className="body">
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Body>
+      </DirectoryItemContainer>
+    );
 }
 
 export default DirectoryItem;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,36 @@
 import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss'
 
-const Directory = ({categories}) => {
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: ''
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "women",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "men",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+const Directory = () => {
     return (
       <div className="directory-container">
         {categories.map((category) => (
