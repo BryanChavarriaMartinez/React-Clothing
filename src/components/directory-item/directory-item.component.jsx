d1623c97fe7dc5e632c03715ad13666507e8ede6
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-06-29 14:30:44 -0600

    Project finished

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+    const onNavigateHandler = () => navigate(route);
 
     return (
-      <DirectoryItemContainer>
+      <DirectoryItemContainer onClick={onNavigateHandler}>
         <BackgroundImage className="background-image" imageUrl={imageUrl} />
         <Body className="body">
           <h2>{title}</h2>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,27 +6,31 @@ const categories = [
     id: 1,
     title: "hats",
     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    route: ''
+    route: "shop/hats",
   },
   {
     id: 2,
     title: "jackets",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
   },
   {
     id: 3,
     title: "sneakers",
     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers",
   },
   {
     id: 4,
-    title: "women",
+    title: "womens",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens",
   },
   {
     id: 5,
-    title: "men",
+    title: "mens",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens",
   },
 ];
 
