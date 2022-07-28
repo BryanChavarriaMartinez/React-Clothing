fb61032dea28192c69700aba942d612a5382a6c6
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-07-28 14:06:13 -0600

    changed to production from dev 2nd time

--- src/store/store.ts
+++ src/store/store.ts
@@ -31,12 +31,12 @@ const sagaMiddleware = createSagaMiddleware();
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
-  process.env.NODE_ENV !== 'production' && logger,
+  process.env.NODE_ENV === 'production' && logger,
   sagaMiddleware,
 ].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV === 'production' &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
