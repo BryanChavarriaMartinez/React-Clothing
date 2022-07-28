a9ee4e37224a274d7d4b374ed79eb8b839f9eb47
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-07-28 16:30:59 -0600

    changes on the indes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Elements } from "@stripe/react-stripe-js";
-
-import App from "./App";
 import { store, persistor } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
-
 import "./index.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 
